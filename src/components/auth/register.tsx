6367a19535d76d83014b550fd190156d6a0987cf
Santiago Rodriguez <santiagodrm@gmail.com>
2025-07-28 01:21:39 +0200

    pages: Login and Register

---
+++ src/components/auth/register.tsx
@@ -0,0 +1,22 @@
+import './authStyles.css'
+
+export default function Register(){
+    return(
+        <div className="auth-container">
+            <div className="auth-content">
+                <span>Register to TaskMaster</span>
+                <form action="POST">
+                    <div className="input-row">
+                        <input type="text" name="firstName" id="firstName-input" placeholder="First name"/>
+                        <input type="text" name="lastName" id="lastName-input" placeholder="Last name"/>
+                    </div>
+                    <input type="email" name="email" id="email-input" placeholder="Email"/>
+                    <input type="password" name="password" id="password-input" placeholder="Password"/>
+                    <input type="confirmPassword" name="confirmPassword" id="confirmPassword-input" placeholder="Confirm password"/>
+                    <button type="submit" id="submit-btn">Register</button>
+                </form>
+                <a href="#" id="login-btn">You already have an account?</a>
+            </div>
+        </div>
+    );
+}
